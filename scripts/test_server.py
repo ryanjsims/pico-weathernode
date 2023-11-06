baf5d287a73e4e4f7268f7cfcebf9a156bc58846
Ryan Sims <ryanjsims@arizona.edu>
2023-11-05 19:13:01 -0700

    Add basic test socketio server

---
+++ scripts/test_server.py
@@ -0,0 +1,13 @@
+import socketio
+import eventlet
+import eventlet.wsgi
+
+sio = socketio.Server()
+
+@sio.event
+def weather_event(sid, data):
+    print(f"From {sid}: {data}")
+
+if __name__ == "__main__":
+    app = socketio.WSGIApp(sio)
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
