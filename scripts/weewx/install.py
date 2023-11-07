6c662af854d5976eea95f8d59c04dc234d8e29f9
Ryan Sims <ryanjsims@arizona.edu>
2023-11-06 21:31:16 -0700

    Add requirements to installer

--- scripts/weewx/install.py
+++ scripts/weewx/install.py
@@ -1,4 +1,5 @@
 from weecfg.extension import ExtensionInstaller
+import setuptools
 
 def loader():
     return WeathernodeInstaller()
@@ -22,3 +23,15 @@ class WeathernodeInstaller(ExtensionInstaller):
             },
             files=[('bin/user', ['bin/user/weathernode_driver.py'])]
         )
+        setuptools.setup(install_requires=[
+            "bidict==0.22.1",
+            "dnspython==2.4.2",
+            "eventlet==0.33.3",
+            "greenlet==3.0.1",
+            "h11==0.14.0",
+            "python-engineio==4.8.0",
+            "python-socketio==5.10.0",
+            "simple-websocket==1.0.0",
+            "six==1.16.0",
+            "wsproto==1.2.0"
+        ])
