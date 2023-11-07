0c433cb1befba220bdaec7e20fa5ed3f73d6562c
Ryan Sims <ryanjsims@arizona.edu>
2023-11-06 21:38:24 -0700

    Remove setuptools stuff

--- scripts/weewx/install.py
+++ scripts/weewx/install.py
@@ -1,5 +1,4 @@
 from weecfg.extension import ExtensionInstaller
-import setuptools
 
 def loader():
     return WeathernodeInstaller()
@@ -23,15 +22,3 @@ class WeathernodeInstaller(ExtensionInstaller):
             },
             files=[('bin/user', ['bin/user/weathernode_driver.py'])]
         )
-        setuptools.setup(install_requires=[
-            "bidict==0.22.1",
-            "dnspython==2.4.2",
-            "eventlet==0.33.3",
-            "greenlet==3.0.1",
-            "h11==0.14.0",
-            "python-engineio==4.8.0",
-            "python-socketio==5.10.0",
-            "simple-websocket==1.0.0",
-            "six==1.16.0",
-            "wsproto==1.2.0"
-        ])
